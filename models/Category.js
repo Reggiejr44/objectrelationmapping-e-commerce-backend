3d1dd0a5e7472f834e16173f5abba9eda89a4418
Reggiejr44 <115569377+Reggiejr44@users.noreply.github.com>
2023-02-03 23:44:54 -0500

    completed model folder that creates and imports models, defines columns, plus more.

--- models/Category.js
+++ models/Category.js
@@ -2,11 +2,22 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
+//creates category models 
 class Category extends Model {}
 
 Category.init(
+  // defining columns
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
